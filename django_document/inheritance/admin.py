c35855c2bbab08931f16ea7a7e9397f4c9be2a61
Kyeong Lee <kaythechemist@gmail.com>
2017-10-11 17:41:50 +0900

    proxy added

--- django_document/inheritance/admin.py
+++ django_document/inheritance/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Student, Teacher, School, Place, Restaurant
+from .models import Student, Teacher, School, Place, Restaurant, Champion
 
 admin.site.register(School)
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Place)
 admin.site.register(Restaurant)
+admin.site.register(Champion)
