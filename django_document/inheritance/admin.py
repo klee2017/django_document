53b13327692e08cc2c932c69b1320785a985156d
Kyeong Lee <kaythechemist@gmail.com>
2017-10-11 16:37:13 +0900

    adminpy forgotten

--- django_document/inheritance/admin.py
+++ django_document/inheritance/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Student, Teacher, School
+from .models import Student, Teacher, School, Place, Restaurant
 
 admin.site.register(School)
 admin.site.register(Student)
 admin.site.register(Teacher)
+admin.site.register(Place)
+admin.site.register(Restaurant)
--- django_document/inheritance/models/multi_table.py
+++ django_document/inheritance/models/multi_table.py
@@ -10,7 +10,13 @@ class Place(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=80)
 
+    def __str__(self):
+        return self.name
+
 
 class Restaurant(Place):
     serves_hot_dogs = models.BooleanField(default=False)
     serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name} restaurant'
